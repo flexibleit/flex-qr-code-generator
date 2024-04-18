680a8e95bec038e4279a1cd062928fc671321f34
Shovickbarua <shovickbarua92@gmail.com>
2024-04-18 16:50:37 +0600

    update

--- inc/flexqr-settings.php
+++ inc/flexqr-settings.php
@@ -18,18 +18,18 @@ if (!function_exists('flexqr_code_settings')){
             <h2 style="color:white; ">FlexQR Code Generator</h2>
             </div>
             <form method="post" action="">
-                <table style='border-collapse: collapse; width: 20%;'>
+                <table style='border-collapse: collapse; width: 18%;'>
                     <tr>
-                        <td style='padding: 15px; text-align: left;'><label for="download_button" style="font-size: 15px; font-weight: 500;">Show QrCode download button:</label></td>
-                        <td style='padding: 15px; text-align: left;'><input type="checkbox" name="download_button" value="1" <?php checked( $flexqr_settings['download_button'], '1' ); ?>></td>
+                        <td style='padding: 15px 0; text-align: left;'><input type="checkbox" name="download_button" value="1" <?php checked( $flexqr_settings['download_button'], '1' ); ?>></td>
+                        <td style='padding: 15px 0; text-align: left;'><label for="download_button" style="font-size: 15px; font-weight: 500;">Show QrCode download button:</label></td>
                     </tr>
                     <tr>
-                        <td style='padding: 15px; text-align: left;'><label for="content" style="font-size: 15px; font-weight: 500;">Show QrCode Content:</label></td>
-                        <td style='padding: 15px; text-align: left;'><input type="checkbox" name="content" value="1" <?php checked( $flexqr_settings['content'], '1' ); ?>></td>
+                        <td style='padding: 15px 0; text-align: left;'><input type="checkbox" name="content" value="1" <?php checked( $flexqr_settings['content'], '1' ); ?>></td>
+                        <td style='padding: 15px 0; text-align: left;'><label for="content" style="font-size: 15px; font-weight: 500;">Show QrCode Content:</label></td>
                     </tr>
                 </table>
                 <?php wp_nonce_field( 'flexqr_nonce_action', 'flexqr_nonce' ); ?>
-                <button type="submit" name="submit_flexqr_settings" style="background-color: #2271b1; color: white; padding: 8px 18px; border: none; border-radius: 4px; cursor: pointer; margin-left: 15px;" >Submit</button>
+                <button type="submit" name="submit_flexqr_settings" style="background-color: #2271b1; color: white; padding: 8px 18px; border: none; border-radius: 4px; cursor: pointer;" >Submit</button>
             </form>
         </div>
         <?php
